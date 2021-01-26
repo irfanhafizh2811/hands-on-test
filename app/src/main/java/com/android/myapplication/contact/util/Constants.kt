deb001ab0d0da40c672f3893aaa51fe7e3f0c18d
irfan hafizh tawakal <irfanhafizh2811@gmail.com>
2021-01-26 16:49:44 +0700

    plugin dagger hilt

--- app/src/main/java/com/android/myapplication/contact/util/Constants.kt
+++ app/src/main/java/com/android/myapplication/contact/util/Constants.kt
@@ -1,12 +1,6 @@
 
 const val BASE_URL = "https://randomuser.me/"
 const val NETWORK_TIMEOUT = 3000L
-const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/"
-const val IMAGE_FILE_SIZE = "w185"
-const val IMAGE_BACKDROP_FILE_SIZE = "w780"
-const val YOUTUBE_BASE_URL = "https://www.youtube.com/watch?v="
-const val YOUTUBE_THUMBNAIL_BASE_URL = "https://img.youtube.com/vi/"
-const val YOUTUBE_THUMBNAIL_URL_JPG = "/0.jpg"
 const val PAGE_INFO = 0
 const val PAGE_VIDEOS= 1
 const val PAGE_REVIEW =2
